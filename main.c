d979a736e7ae5832b7baeb2c77aa633f6fd20a1a
TheoryCode <167247288+TheoryCode@users.noreply.github.com>
2024-04-16 19:00:49 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int numero;//essa variável se tornou uma variável global
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+    printf("Sistemas\n\n");
+    printf("1-Windows\n");
+    printf("2-Linux\n");
+    printf("\nEscolha a opção desejada\n");
+    scanf("%d", &numero);
+    switch(numero) //estrutura Switch case
+    {
+    case 1:
+        system("cls");
+        printf("iniciando o Windows...\n");
+        break; //termina o case 1
+    case 2:
+         system("cls");
+        printf("iniciando o Linux...\n");
+        break; //termina o case 2
+
+    default:// para tudo que for diferente de case 1 e 2
+        printf("Opção inválida");
+    }
+    return 0;
+}
